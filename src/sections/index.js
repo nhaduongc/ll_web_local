a7cb5c787a23a993bd83b180e79f19fbdfba8269
d-pcy <danielpacey@protonmail.com>
2022-07-11 15:40:00 +0100

    - Connected to local graphql api and added query for page builder sections on the homepage - Iterated over page builder sections and rendered component for each section

---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import FullWidthText from "./FullWidthText.astro"
+import Hero from "./Hero.astro"
+import Image from "./Image.astro"
+import PreviousWinners from "./PreviousWinners.astro"
+import TwoColumnText from "./TwoColumnText.astro"
+
+export {
+  FullWidthText,
+  Hero,
+  Image,
+  PreviousWinners,
+  TwoColumnText,
+}
