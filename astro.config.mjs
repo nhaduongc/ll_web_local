48e395a6021990b7a5efc8eba7e4a10af0e55c11
d-pcy <danielpacey@protonmail.com>
2022-09-28 16:34:47 +0100

    Started SEO and social media stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
 import svelte from "@astrojs/svelte";
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+
+const stagingConfig = {
+  policy: [
+    {
+      userAgent: 'Twitterbot',
+      disallow: '',
+    },
+    {
+      userAgent: '*',
+      disallow: '/',
+    },
+  ],
+}
+
+const productionConfig = {}
+
+const isProd = !!import.meta.env.MODE === 'production'
+
+const siteConfig = isProd ? productionConfig : stagingConfig
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), svelte()]
+  experimental: {
+    integrations: true,
+  },
+  integrations: [tailwind(), svelte(), sitemap(), robotsTxt(siteConfig)]
 });
