1629c684bc28c4d95b9649236a76bfe822f0fc1e
Jason Kneen <502002+jasonkneen@users.noreply.github.com>
2023-09-07 10:18:47 +0100

    Update for Android redirects for binId

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,13 +1,11 @@
 import { FC, Fragment, ReactNode, useEffect, useState } from 'react';
 import { useIntercom } from 'react-use-intercom';
-
+import Footer from 'src/components/footer';
 import { Head } from 'src/components/head';
 import { Header } from 'src/components/header';
-import Footer from 'src/components/footer';
 import { FooterProps } from 'src/types/page-props/footer';
 import { HeadProps } from 'src/types/page-props/head';
 import { HeaderProps } from 'src/types/page-props/header';
-
 export const MainLayout: FC<{
     children: ReactNode;
     props: {
@@ -17,30 +15,30 @@ export const MainLayout: FC<{
     };
 }> = ({ children, props }) => {
     const [menuOpen, setMenuOpen] = useState(false);
-
     useEffect(() => {
+        if (/Android/i.test(navigator.userAgent)) {
+            // Use a timeout to handle the 'Navigation is cancelled' error on Android
+            setTimeout(() => {
+                window.location.href = 'intent://#Intent;package=com.litterlotto.app;end;'; // Replace with your Play Store link
+            }, 250);
+            window.location.href = 'litterlotto://'; // Replace with your URI
+        }
         if (menuOpen) {
             document.body.classList.add('overflow-hidden');
             return;
         }
-
         document.body.classList.remove('overflow-hidden');
     }, [menuOpen]);
-
     const { boot } = useIntercom();
-
     if (typeof window !== 'undefined') {
         boot();
     }
-
     return (
         <Fragment>
             <Head head={props.head} />
             <main>
                 <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} header={props.header} />
-
                 {children}
-
                 <Footer footer={props.footer} />
             </main>
         </Fragment>
