1738205fb7236c06158566e38b6545e9b33ef87e
Jason Kneen <502002+jasonkneen@users.noreply.github.com>
2023-09-07 10:56:47 +0100

    Update main-layout.tsx

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -1,11 +1,13 @@
 import { FC, Fragment, ReactNode, useEffect, useState } from 'react';
 import { useIntercom } from 'react-use-intercom';
+
 import Footer from 'src/components/footer';
 import { Head } from 'src/components/head';
 import { Header } from 'src/components/header';
 import { FooterProps } from 'src/types/page-props/footer';
 import { HeadProps } from 'src/types/page-props/head';
 import { HeaderProps } from 'src/types/page-props/header';
+
 export const MainLayout: FC<{
     children: ReactNode;
     props: {
@@ -15,6 +17,7 @@ export const MainLayout: FC<{
     };
 }> = ({ children, props }) => {
     const [menuOpen, setMenuOpen] = useState(false);
+
     useEffect(() => {
         if (/Android/i.test(navigator.userAgent)) {
             // Use a timeout to handle the 'Navigation is cancelled' error on Android
@@ -29,18 +32,24 @@ export const MainLayout: FC<{
             document.body.classList.add('overflow-hidden');
             return;
         }
+
         document.body.classList.remove('overflow-hidden');
     }, [menuOpen]);
+
     const { boot } = useIntercom();
+
     if (typeof window !== 'undefined') {
         boot();
     }
+
     return (
         <Fragment>
             <Head head={props.head} />
             <main>
                 <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} header={props.header} />
+
                 {children}
+
                 <Footer footer={props.footer} />
             </main>
         </Fragment>
